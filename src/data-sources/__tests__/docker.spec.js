610925c9ea807230e0e5a032e85567302084bfe7
Matthew Palmer <mspalmer91@gmail.com>
2020-07-26 14:44:12 -0400

    fix docker unit tests

--- src/data-sources/__tests__/docker.spec.js
+++ src/data-sources/__tests__/docker.spec.js
@@ -1,27 +1,23 @@
 'use strict';
 
-const DockerDatasource = require('../docker');
+const Docker = require('../docker');
 
 jest.mock('docker-cli-js', () => ({
 	Docker: class MockDocker {
 		constructor() {
-			this.command = jest.fn();
+			this.cli = jest.fn();
 		}
 	},
 }));
+
 jest.mock('../../constants', () => ({ IMAGE_NAME: 'mockImageName' }));
 
-describe('#list', () => {
-	test('Filters out all images not of IMAGE_NAME', async () => {
-		const datasource = new DockerDatasource();
-		datasource.docker.command.mockResolvedValue([
-			{ id: 'a', image: 'mockImageName' },
-			{ id: 'b', image: 'notMockImageName' },
-			{ id: 'c', image: 'mockImageName' },
-		]);
-		return expect(datasource.list()).resolves.toEqual([
-			{ id: 'a', image: 'mockImageName' },
-			{ id: 'c', image: 'mockImageName' },
-		]);
-	});
+const originalEnv = { ...process.env };
+afterEach(() => {
+	process.env = { ...originalEnv };
+});
+
+test('toContainerName', () => {
+	process.env.CONTAINER_NAMESPACE = 'swag';
+	expect(Docker.toContainerName('master')).toBe('swag_master');
 });
